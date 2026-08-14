e505e789f90f0e8a11fe45a1545d322f055ca406
bjohnson11719@gmail.com <bjohnson11719@gmail.com>
2026-08-14 22:59:01 +0000

    this should work now...

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,19 +1,19 @@
 
 /*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
+Updated this to make an order view
 */
 
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
 with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
+	select
+		id as order_id,
+		user_id as customer_id,
+		order_date,
+		status
+
+	from `dbt-tutorial.jaffle_shop.orders`
 
 )
 
